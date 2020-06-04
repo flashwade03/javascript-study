8e3b35c74b1155cb9f1b53740a7ed0fe21238d44
hcjung <hcjung@adminui-MacBookPro.local>
2020-06-05 08:23:16 +0900

    first day

---
+++ day01/syntax/conditional.js
@@ -0,0 +1,11 @@
+var args = process.argv;
+console.log(args);
+console.log(args.slice(2));
+console.log('A');
+console.log('B');
+if (false) {
+  console.log('C1');
+} else {
+  console.log('C2');
+}
+console.log('D');
